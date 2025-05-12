26a004f959bb1ab189015a72edd3d548c737d1a8
Prish20 <contact@christadriansanya.me>
2025-05-12 03:25:59 +0300

    Add new SVG icons and images; remove unused SVG files
    - Added new SVG icons: logout, message, record, search, smiley, star, upload, and video.
    - Added new images: alex.png, checked.png, david.png, dummy.jpg, emily.png, jason.png, jessica.png, lisa.png, michael.png, sarah.png, video1.png.
    - Added new sample thumbnails and video files.
    - Created a new TypeScript configuration file (tsconfig.json).
    - Removed unused SVG files: file.svg, globe.svg, next.svg, vercel.svg, and window.svg.

---
+++ client/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-karla">
+                Welcome to Loomify
+            </h1>
+        </div>
+    )
+}
+
+export default Page
